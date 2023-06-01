a35bc505935690e5cb25cf1772ac182a8f60306f
thiagocarvalho93 <thiagopaes1993@gmail.com>
2023-06-01 16:09:29 -0300

    🏗️ Basic site layout

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,6 +1,47 @@
 <template>
-  <div>
-    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <div class="isolate bg-gray-950 h-screen px-6 py-24 sm:py-32 lg:px-8">
+    <div class="mx-auto max-w-2xl text-center">
+      <h2 class="text-3xl font-bold tracking-tight text-white sm:text-4xl">Short URL</h2>
+      <p class="mt-2 text-lg leading-8 text-gray-600">Generate short urls for your links.</p>
+    </div>
+    <form action="#" method="POST" class="mx-auto mt-16 max-w-xl sm:mt-20">
+      <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+        <div class="sm:col-span-2">
+          <label for="search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+            >Search</label
+          >
+          <div class="relative">
+            <input
+              type="search"
+              id="search"
+              class="block w-full p-4 pr-28 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Paste here your long URL"
+              v-model="longUrl"
+              required
+            />
+            <button
+              class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              @click="handleClick"
+            >
+              Generate
+            </button>
+          </div>
+        </div>
+      </div>
+    </form>
   </div>
 </template>
-  
+
+<script>
+export default {
+  name: "IndexPage",
+  data() {
+    return {
+      longUrl: "",
+    };
+  },
+  methods: {
+    async handleClick() {},
+  },
+};
+</script>
