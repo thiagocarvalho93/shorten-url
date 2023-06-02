856c9fa095c5d466b806b64525a900283ced6259
thiagocarvalho93 <thiagopaes1993@gmail.com>
2023-06-01 22:10:40 -0300

    :lipstick: UI Adjustments

--- frontend/app.vue
+++ frontend/app.vue
@@ -2,20 +2,20 @@
   <div class="isolate h-screen px-6 py-24 sm:py-32 lg:px-8">
     <div class="mx-auto max-w-2xl text-center">
       <h2 class="text-3xl font-bold tracking-tight text-white sm:text-4xl">✂ShortURLs</h2>
-      <p class="mt-2 text-lg leading-8 text-gray-600">Generate short urls for your links.</p>
+      <p class="mt-2 text-lg leading-8 text-gray-600">Generate short urls for your long links!</p>
     </div>
     <form action="#" method="POST" class="mx-auto mt-16 max-w-xl sm:mt-20">
       <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
         <div class="sm:col-span-2">
           <div class="relative mb-5">
             <input
-              class="block w-full p-4 pr-28 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              class="block w-full p-4 pr-28 text-sm text-gray-900 border border-gray-300 rounded-md bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Paste here your long URL"
               v-model="longUrl"
               required
             />
             <button
-              class="text-white w-20 absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              class="text-white w-20 absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-md text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               type="button"
               @click="handleGenerate"
             >
@@ -24,14 +24,12 @@
           </div>
           <div v-if="generatedUrl.token" class="relative">
             <input
-              class="block w-full p-4 pr-28 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-900 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Paste here your long URL"
+              class="block w-full p-4 pr-28 text-sm text-gray-900 border border-gray-300 rounded-md bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-900 dark:border-gray-800 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               v-model="completeGeneratedUrl"
-              required
               readonly
             />
             <button
-              class="text-white w-20 absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              class="text-blue-700 w-20 absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-md text-sm px-4 py-2 dark:bg-gray-200 dark:hover:bg-gray-300 dark:focus:ring-blue-800"
               type="button"
               @click="handleCopy"
             >
@@ -68,13 +66,15 @@ export default {
         this.generatedUrl = data;
         console.log(data);
       } catch (e) {
+        const message = e.data.errors; //TODO
         console.log(e.data.errors);
+        alert(message);
       }
     },
 
     handleCopy() {
       navigator.clipboard.writeText(this.completeGeneratedUrl);
-      alert("Copied to clipboard!")
+      alert("Copied to clipboard!");
     },
 
     async postUrl() {
