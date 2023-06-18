ccc9eb5e3720b8158c6d3440290eec13848c590c
thiagocarvalho93 <thiagopaes1993@gmail.com>
2023-06-18 11:35:29 -0300

    🦺 url validation and pointing to deployed api

--- frontend/app.vue
+++ frontend/app.vue
@@ -19,7 +19,7 @@
               class="text-white w-20 absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-md text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 dark:disabled:bg-gray-400"
               type="button"
               @click="handleGenerate"
-              :disabled="longUrl.length < 1"
+              :disabled="invalidUrl"
             >
               Go
             </button>
@@ -63,16 +63,22 @@ export default {
 
   computed: {
     completeGeneratedUrl() {
-      return this.generatedUrl.token ? this.baseApiUrl + this.generatedUrl.token : "";
+      return this.generatedUrl.token ? `${this.baseApiUrl}/${this.generatedUrl.token}` : "";
     },
+
+    invalidUrl() {
+      const urlRegex = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/
+      return this.longUrl.length < 1 || !this.longUrl.match(urlRegex);
+    }
   },
   methods: {
     async handleGenerate() {
       try {
         const data = await this.postUrl();
+        console.log(data);
         this.generatedUrl = data;
       } catch (e) {
-        const message = e.data.errors; //TODO
+        const message = e.data?.errors ?? "Error connecting to service!"; //TODO
         alert(message);
       }
     },
--- frontend/constants.js
+++ frontend/constants.js
@@ -1,1 +1,1 @@
-export const API_BASE_URL = "http://localhost:5190/";
+export const API_BASE_URL = "https://shorten-url-api.onrender.com";
